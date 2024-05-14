79e3b5da800eaedeacc76badb23f5ba7052f20d0
Sean Dolan <sdolan366@gmail.com>
2024-05-14 12:08:28 +1000

    Set up database details

--- server/app.js
+++ server/app.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const cors = require('cors')
 const paymentsRouter = require('./routes/payments')
 const incomeRouter = require('./routes/income')
--- server/db.js
+++ server/db.js
@@ -1,9 +1,9 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'postgres',
-    host: 'localhost',
+    user: 'snowballdb_user',
+    password: 'I4y3k5c0VpoVQqfgKdzt5U00Cw37beeh',
+    host: 'postgres://snowballdb_user:I4y3k5c0VpoVQqfgKdzt5U00Cw37beeh@dpg-cp1c9ugl6cac738olj90-a.singapore-postgres.render.com/snowballdb?ssl=true',
     database: 'snowballdb',
     port: 5432
 })
